04738da4e5fcc33cecc84564af582499caaa398f
Marcio Trindade <marciotrindade@gmail.com>
2009-01-20 09:23:43 -0200

    add named_scope to pages

--- controllers/admin/pages_controller.php
+++ controllers/admin/pages_controller.php
@@ -2,16 +2,15 @@
 class PagesController extends AdminController {
 	var $name = 'Pages';
 
-	function index($parent_id=0)
+	function index($parent_id=false)
 	{
-		if (!isset($this->order))
+		if ($parent_id)
 		{
-			$this->order = $this->{$this->modelClass}->alias.".position";
+			$collection = $this->Page->find("all", array("conditions" => array("Page.parent_id" => $parent_id), "order" => "Page.position"));
+		}else{
+			$collection = $this->Page->root("all", array("order" => "Page.position"));
 		}
 
-		$conditions = array($this->{$this->modelClass}->alias . ".parent_id" => $parent_id);
-
-		$collection = $this->{$this->modelClass}->find("all", array("conditions" => $conditions, "order" => $this->order));
 		$this->set(compact("collection"));
 
 		// render template
--- models/page.php
+++ models/page.php
@@ -12,6 +12,16 @@ class Page extends AppModel {
 	var $belongsTo = array("Parent" => array("className" => "Page", "foreignKey" => "parent_id", "counterCache" => true));
 	var $hasMany = array("Childrens" => array("className" => "Page", "foreignKey" => "parent_id", "order" => "position"));
 	
-	var $actsAs = array("Permalink", "Containable");
+	var $actsAs = array(
+		"Permalink",
+		"Containable",
+			"NamedScope" => array(
+				"root" => array(
+					"conditions" => array(
+						"Page.parent_id" => 0
+					)
+				)
+			)
+	);
 }
 ?>
