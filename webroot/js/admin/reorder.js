fcf19c0911038cb955dfb67299b7bb5c6f6812fa
Marcio Trindade <marciotrindade@gmail.com>
2009-07-23 09:52:31 -0300

    update jequery-ui to jquery 1.3.2

--- webroot/js/admin/reorder.js
+++ webroot/js/admin/reorder.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	$("#order").sortable({axis:'y',dropOnEmpty:false,update:function(){$.ajax({complete:function(request){$('#order').effect('highlight');}, data:$(this).sortable('serialize',{key:'order'}), dataType:'script', type:'post', url:$("#url_order").attr("href")})}})	
+	$("#order").sortable({
+		axis:'y',
+		dropOnEmpty:false,
+		update:function(){$.ajax({complete:function(request){$('#order').effect('highlight');},
+		data:$(this).sortable('serialize',{key:'order[]'}),
+		dataType:'script',
+		type:'post',
+		url:$("#url_order").attr("href")
+	})}});
 });
